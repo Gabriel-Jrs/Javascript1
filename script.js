cb99012e3a9ba8ecd86556247f850eead1c2893c
Gabriel José <gabriell4924462@gmail.com>
2025-08-05 17:34:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('olá mundo1!')
+
+let x = 10
+{
+    {
+        let y = '5'
+        {
+            let a = 1
+            console.log(y=x)
+            console.log()
+        }
+    }
+}
